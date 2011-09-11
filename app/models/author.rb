1ae6e113f0ff0f4f1b6771e9b4c33ac3b946aae2
Jordi Polo <jordi@cookpad.com>
2011-09-11 18:32:26 +0900

    only interested in non-merge commits + method  to rank authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,14 @@
 class Author < ActiveRecord::Base
-  
+
   def current_problems_in(project)
     project.commits.recent.first.problems.where(:author_id => self)
   end
-  
+
   def self.find_or_create_from_metadata(metadata)
     find_or_create_by_name_and_email(:name => metadata.name, :email => metadata.email)
+  end
+
+  def total_contributed_in(project, metric)
+    project.commits.where(:author_id => self).inject(0){|sum,commit| sum + commit.change(metric)}
   end
 end
