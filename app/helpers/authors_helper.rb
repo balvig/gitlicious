ad5da303585d0b16d3bb2847deaf1a1a6180cb5e
Jens Balvig <jens@balvig.com>
2012-01-11 08:38:36 +0900

    Removed unused helper

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -3,8 +3,4 @@ module AuthorsHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&r=PG&d=wavatar"
   end
-
-  def genitive(str)
-    str+= str[str.length-1,1] == 's' ? "'" : "'s" if str.present?
-  end
 end
